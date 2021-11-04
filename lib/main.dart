361a13ea7887e1964254deef5f8ec101f01a3d4f
zahir_mendaci <zahir@cleanmob.eu>
2021-11-04 14:35:49 +0100

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:paiement_app/payment.dart';
+import 'package:stripe_payment/stripe_payment.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    StripeServices.init();
+  }
+
+  void payNow() async {
+    //the amount must be transformed to cents
+    var response =  await StripeServices.payNowHandler(amount: '10000', currency: 'EUR');
+    print('response message ${response.message}');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: ElevatedButton(
+            onPressed: (){
+              payNow();
+            },
+            child: Text("Payer")
+        )
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
